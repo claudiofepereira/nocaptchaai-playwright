69a3348a59b0d727fe7b82510c87ab421cd1a88b
claudiofepereira <claudiopereira@ipvc.pt>
2023-04-11 22:46:13 +0100

    Updated description.

--- setup.py
+++ setup.py
@@ -1,21 +1,35 @@
 from setuptools import setup, find_packages
 
+"""nocaptchaai-playwright
+
+nocaptchaai-playwright is a Python library that uses Playwright and NoCaptchaAI.com API to solve hCaptcha challenges.
+
+An example of how to use this library can be found in examples/nopecha_solver_example.py.
+
+"""
+
+DOCLINES = (__doc__ or "").split("\n")
+
 setup(
     name="nocaptchaai_playwright",
-    version="0.0.1",
+    version="0.0.2",
     description="Playwright implementation of the NoCaptchaAI API",
+    long_description="\n".join(DOCLINES[2:]),
     url="https://github.com/claudiofepereira/nocaptchaai-playwright",
     author="Cláudio Pereira",
     license="MIT",
-    packages=find_packages(),
+    packages=["nocaptchaai_playwright"],
     install_requires=[
-        "playwright>=1.32.1",
-        "requests>=2.28.2",
+        "playwright",
+        "requests",
     ],
     classifiers=[
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
+        "Natural Language :: English",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.10",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
 )
